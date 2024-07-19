b465c62e0d01746f12ea68aa148f4e404dd086da
Jatin-Giri <jatin14038@gmail.com>
2024-07-19 22:01:24 +0530

    Adding New Code TO All

---
+++ Script/0_main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn");
+let hide = document.querySelector(".hide");
+// foucus
+let find = document.getElementById("find");
+var mask = document.getElementById("mask");
+
+btn.style.left = (Math.random() * 100) + "%";
+btn.style.top = (Math.random() * 100) + "%";
+
+
+
+
+
+
